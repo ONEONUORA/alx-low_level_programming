f14b026a9f3346bcf94f3e67e5390c6655c56140
ONEONUORA <monweebube44@gmail.com>
2023-11-03 20:14:12 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,46 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file.
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
  *
- * Return: 1 on success, -1 on failure.
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_d, lenw;
-    
-    if (filename == NULL)
-        return (-1);
+	int count = 0;
+	int fd = 0;
+	int output = 0;
 
-    file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    
-    if (file_d == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        lenw = write(file_d, text_content, _strlen(text_content));
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-        if (lenw == -1 || (size_t)lenw != (size_t)_strlen(text_content))
-        {
-            close(file_d);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
 
-    close(file_d);
-    return (1);
+	output = write(fd, text_content, count);
+	if (output == -1 || output != count)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
-
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,41 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: The name of the file to append to.
- * @text_content: The text content to add to the file.
+ * append_text_to_file - function with two arguments
+ * @filename: pointer to name of file
+ * @text_content: pointer to string content
  *
- * Return: 1 on success, -1 on failure.
+ * Description: function that appends to text at the end of a file
+ * Return: 1 on success or -1 for failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file_d, lenw;
+	int count = 0;
+	int output = 0;
+	int fd = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename != NULL)
+		fd = open(filename, O_WRONLY | O_APPEND);
+	else
+		return (-1);
+	if (fd == -1)
+		return (-1);
 
-    file_d = open(filename, O_WRONLY | O_APPEND);
-    
-    if (file_d == -1)
-        return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+		return (1);
 
-    if (text_content != NULL)
-    {
-        lenw = write(file_d, text_content, _strlen(text_content));
-
-        if (lenw == -1 || (size_t)lenw != (size_t)_strlen(text_content))
-        {
-            close(file_d);
-            return (-1);
-        }
-    }
-
-    close(file_d);
-    return (1);
+	output = write(fd, text_content, count);
+	if (output == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
